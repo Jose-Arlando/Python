9d361927d841a549f2a936a5e8083fc15b151b4a
José Arlando <arlandompm@gmail.com>
2026-08-03 18:09:57 -0400

    estrutura condicional

--- Base/estruturas.py
+++ Base/estruturas.py
@@ -1,2 +1,8 @@
-print("olá")
-print("Tudo bem")
+#Estrutura de condição
+idade = int(input("Qual sua idade? "))
+if idade > 18:
+    print("Maior de idade. Já pode ser preso!")
+elif idade == 18:
+    print("Fez aniversário esse ano pivete, parabéns!")
+else:
+    print("Menor de idade")
