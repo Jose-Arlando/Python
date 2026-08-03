66430521e5ee5301566a102692e565001751e671
José Arlando <arlandompm@gmail.com>
2026-08-03 17:56:09 -0400

    editado

--- Aprendendo.py
+++ Aprendendo.py
@@ -1,1 +1,16 @@
-print("hello")
+print("Olá! Vamos aprender python!")
+
+nome = "José Arlando" #texto
+idade = 19 #valor inteiro
+peso = 60.0 #valor real
+casado = True #boolean
+print("---------------------------------------------------------------")
+print("olá ", nome, "sua idade é ", idade, "e você pesa ", peso, "KG.\nCasado: ", casado)
+print("---------------------------------------------------------------")
+
+nome_namorada = input("Qual o nome da sua namorada? ")
+idade_namorada = int(input("Qual a idade da sua namorada: "))
+altura_namorada = float(input("Qual a altura da sua namorada: "))
+print("---------------------------------------------------------------")
+print("olá ", nome_namorada, "sua idade é ", idade_namorada, "e sua altura é", altura_namorada)
+print("---------------------------------------------------------------")
