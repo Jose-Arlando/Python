bd0e8c86f2a35d9749a991a9c8d961a2aba7a205
José Arlando <arlandompm@gmail.com>
2026-08-11 07:23:23 -0400

    salvar

---
+++ Base/Ensinando.py
@@ -0,0 +1,5 @@
+#Logica de programação
+#entrada
+numero = int(input("Adicione número: "))
+# saída
+print("o número digitado foi: ", numero);
--- Base/estrutura_repeticao.py
+++ Base/estrutura_repeticao.py
@@ -4,13 +4,14 @@ for = para
 while = enquanto
 """
 print("FOR")
+
 for i in range (0, 11):
     print(i)
 print("FIM")
 
 print("WHILE")
 j = 0
-while j<11:
+while j<=11:
     print(j)
     j = j+1
 print("FIM")
