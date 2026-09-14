e3c48450c1698dbd39da9192a0a3ade649b6226f
José Arlando <arlandompm@gmail.com>
2026-09-14 17:55:38 -0400

    add desafio fat

--- Desafios/Fatoreal.py
+++ Desafios/Fatoreal.py
@@ -1,5 +1,9 @@
-n = int(input("qual valor: "))
-acm_mult=1
-for n in range (n,1):
-    acm_mult = acm_mult * n
-print(acm_mult)
+f = int(input("valor: "))
+k = f
+i=1
+acm = 0
+while i<k:
+    f = f * (k - i)
+    i+=1
+print(f)
+    
